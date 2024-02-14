0489f9bb4050da7e505e86e18c636c83e1ef084e
GunaMandadi <gunashekar166@gmail.com>
2024-02-13 23:46:33 -0600

    add

--- src/main/java/com/java/cis/LogoutServlet.java
+++ src/main/java/com/java/cis/LogoutServlet.java
@@ -4,7 +4,7 @@ import javax.servlet.http.*;
 import java.io.IOException;
 public class LogoutServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws IOException {
         HttpSession session = request.getSession(false);
         if (session != null) {
             session.invalidate();
